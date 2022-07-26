b083101e1c9fb8a300b05ee91567d88f318c6d6f
Otávio <otaviovb.developer@gmail.com>
2022-07-26 15:48:00 -0300

    fix(get): fixed `get` return

--- Controllers/ParticipanteController.cs
+++ Controllers/ParticipanteController.cs
@@ -29,15 +29,15 @@ namespace APIEnem.Controllers
             }
             catch (MySqlException)
             {
-                return BadRequest(new Json(new ResultRequest.BADREQUEST400(_dataParticipante.Identificador, new Message("CONTROLLERS:PARTICIPANTES:GET:DATABASES", "Um erro inesperado ocorreu com a database!", "Contate o suporte sobre o erro."))).ToString());
+                return BadRequest(new Json(new ResultRequest.BADREQUEST400(_dataParticipante.Identificador, new Message("CONTROLLERS:PARTICIPANTES:GET:DATABASES", "Um erro inesperado ocorreu com a database!", "Contate o suporte sobre o erro."), _dataParticipante.IdentificadorConnection)).ToString());
             }
             catch (ModelException MEx)
             {
-                return BadRequest(new Json(new ResultRequest.BADREQUEST400(_dataParticipante.Identificador, new Message(MEx.ErrorLocal, MEx.Message, MEx.Action))).ToString());
+                return BadRequest(new Json(new ResultRequest.BADREQUEST400(_dataParticipante.Identificador, new Message(MEx.ErrorLocal, MEx.Message, MEx.Action), _dataParticipante.IdentificadorConnection)).ToString());
             }
             catch(RequestException REx)
             {
-                return NotFound(new Json(new ResultRequest.NOTFOUND404(_dataParticipante.Identificador, new Message(REx.ErrorLocal, REx.Message, REx.Action))).ToString());
+                return NotFound(new Json(new ResultRequest.NOTFOUND404(_dataParticipante.Identificador, new Message(REx.ErrorLocal, REx.Message, REx.Action), _dataParticipante.IdentificadorConnection)).ToString());
             }
         }
     }
