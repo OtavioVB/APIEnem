96fa6967a0ed669a28eb5569c31a2642ec6ce9ac
Otávio <otaviovb.developer@gmail.com>
2022-07-27 17:49:32 -0300

    feat(model): class for requisition return

---
+++ Models/Candidato/ParticipanteData.cs
@@ -0,0 +1,11 @@
+﻿namespace APIEnem.Models.Candidato
+{
+    public class ParticipanteData
+    {
+        public NúmeroInscrição númeroInscrição { get; set; }
+        public Ano AnoDoEnem { get; set; }
+        public FaixaEtária FaixaEtária { get; set; }
+        public Sexo Sexo { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+    }
+}
