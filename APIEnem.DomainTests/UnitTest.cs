06b5e0c7f0f4edfc4b4b03f663538c65da70b083
OtavioVB <otaviovb.developer@gmail.com>
2022-08-07 17:06:38 -0300

    fix(tests): `Implement` and `Fix` Continuous Integration

---
+++ APIEnem.DomainTests/UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIEnem.DomainTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals(true, true);
+        }
+    }
+}
