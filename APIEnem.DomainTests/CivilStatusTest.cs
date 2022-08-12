a1e04892a9ca5617d11b111851070ba05e76c095
OtavioVB <otaviovb.developer@gmail.com>
2022-08-12 12:21:33 -0300

    feat(tests): `Implementation Domain Tests for StatusCivil Class`

---
+++ APIEnem.DomainTests/CivilStatusTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APIEnem.Domain.Models.Candidato;
+using APIEnem.Domain.Models.Interfaces;
+
+
+namespace APIEnem.DomainTests
+{
+
+    [TestClass]
+    public class CivilStatusTest
+    {
+        public class FakeCivilStatusModel : IModelEstadoCivil
+        {
+            public string ConverterCódigoCivilParaTexto(int códigoEstadoCivil)
+            {
+                return "EstadoCivil";
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestCivilStatusBiggerThanFour()
+        {
+            EstadoCivil estadoCivil = new EstadoCivil(5, new FakeCivilStatusModel());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestCivilStatusLessThanZero()
+        {
+            EstadoCivil estadoCivil = new EstadoCivil(-1, new FakeCivilStatusModel());
+        }
+    }
+}
