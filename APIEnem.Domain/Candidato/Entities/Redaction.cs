339f4d7d6b4896ccb19d40906d52a7a426138c46
OtavioVB <otaviovb.developer@gmail.com>
2022-08-29 21:54:24 -0300

    feat(competence): ´implement competence class`

---
+++ APIEnem.Domain/Candidato/Entities/Redaction.cs
@@ -0,0 +1,19 @@
+﻿using APIEnem.Domain.Candidato.ValueObjects;
+
+namespace APIEnem.Domain.Candidato.Entities;
+
+public class Redaction
+{
+    public int GeneralGrade { get; private set; }
+    public Competence FirstCompetence { get; private set; }
+    public Competence SecondCompetence { get; private set; }
+    public Competence ThirdCompetence { get; private set; }
+    public Competence FourthCompetence { get; private set; }
+    public Competence FifthCompetence { get; private set; }
+    public int CompetenceAverage { get; private set; }
+
+    public Redaction()
+    {
+
+    }
+}
---
+++ APIEnem.Domain/Candidato/ValueObjects/Competence.cs
@@ -0,0 +1,17 @@
+﻿namespace APIEnem.Domain.Candidato.ValueObjects;
+
+public class Competence
+{
+    public int Value { get; private set; }
+
+    public Competence(int value)
+    {
+        Value = value;
+
+    }
+
+    public override string ToString() 
+    { 
+        return Value.ToString(); 
+    }
+}
