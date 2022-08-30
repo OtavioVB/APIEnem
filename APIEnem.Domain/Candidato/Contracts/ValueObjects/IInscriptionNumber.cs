2c8cbdd01e5e5f834f0261819c5aa56c63fe5498
OtavioVB <otaviovb.developer@gmail.com>
2022-08-29 21:42:53 -0300

    refactor(inscriptionNumber): `refactor abstraction and flunt contracts`

---
+++ APIEnem.Domain/Candidato/Contracts/ValueObjects/IInscriptionNumber.cs
@@ -0,0 +1,6 @@
+﻿namespace APIEnem.Domain.Candidato.Contracts.ValueObjects;
+
+public interface IInscriptionNumber
+{
+    public string Value { get; }
+}
---
+++ APIEnem.Domain/Candidato/ValueObjects/InscriptionNumber.cs
@@ -0,0 +1,16 @@
+﻿using Flunt.Notifications;
+using APIEnem.Domain.Candidato.ValueObjects.Assertions;
+using APIEnem.Domain.Candidato.Contracts.ValueObjects;
+
+namespace APIEnem.Domain.Candidato.ValueObjects;
+
+public class InscriptionNumber : Notifiable, IInscriptionNumber
+{
+    public string Value { get; private set; }
+
+    public InscriptionNumber(string value)
+    {
+        Value = value;
+        Assert(InscriptionNumberAssertion.CreateInscriptionNumberContract(Value));
+    }
+}
