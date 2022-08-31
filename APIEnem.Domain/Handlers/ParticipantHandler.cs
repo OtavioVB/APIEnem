a3318d9a94266090a6b40f3939fbe26f338967dc
OtavioVB <otaviovb.developer@gmail.com>
2022-08-30 22:08:49 -0300

    refactor

--- APIEnem.Domain/Handlers/ParticipantHandler.cs
+++ APIEnem.Domain/Handlers/ParticipantHandler.cs
@@ -1,13 +1,20 @@
 ﻿using APIEnem.Domain.Models.Contracts.Infrascructure.Persistence.Participant;
+using APIEnem.Domain.Models.Contracts.Entities;
+using Flunt.Notifications;
 
 namespace APIEnem.Domain.Handlers;
 
-public class ParticipantHandler
+public class ParticipantHandler : Notifiable
 {
     private readonly IRepositoryParticipant _repositoryParticipant;
 
     public ParticipantHandler(IRepositoryParticipant repositoryParticipant)
     {
         _repositoryParticipant = repositoryParticipant;
+    }
+
+    public IParticipantInformation? Handle()
+    {
+        return null;
     }
 }
