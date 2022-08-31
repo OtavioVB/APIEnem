fb6c906a6ba15da57975a08089edea6188d96c9a
OtavioVB <otaviovb.developer@gmail.com>
2022-08-30 22:38:29 -0300

    fix(dependencies): `fix all dependencies´

--- APIEnem.API/Program.cs
+++ APIEnem.API/Program.cs
@@ -1,10 +1,16 @@
 using APIEnem.Domain.Models.Contracts.Infrascructure.Connection;
+using APIEnem.Domain.Models.Contracts.Infrascructure.Persistence.Participant;
+using APIEnem.Domain.Repositories.Persistence.Participant;
 using APIEnem.Domain.Repositories.Connection;
+using APIEnem.Domain.Models.Contracts.Handlers;
+using APIEnem.Domain.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
 
 builder.Services.AddScoped<IDataConnection, DataConnection>();
+builder.Services.AddTransient<IRepositoryParticipant, RepositoryParticipant>();
+builder.Services.AddTransient<IParticipantHandler, ParticipantHandler>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
