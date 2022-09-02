8ef967a50831e8f72a853b0472d93b1456598f64
OtavioVB <otaviovb.developer@gmail.com>
2022-09-01 21:48:21 -0300

    fix(agegroup:tests): `verify message for invalid numbers`

--- APIEnem.DomainTests/Domain/Models/Objects/AgeGroupTests.cs
+++ APIEnem.DomainTests/Domain/Models/Objects/AgeGroupTests.cs
@@ -24,4 +24,13 @@ public class AgeGroupTests
         IAgeGroup ageGroup = new AgeGroup(value);
         Assert.IsTrue(ageGroup.IsValid);
     }
+
+    [TestMethod]
+    [TestCategory("Age Group Validations")]
+    [DataRow(1000), DataRow(21), DataRow(0)]
+    public void VerifyUnexpectedAgeGroupNameForInvalidNumber(int value)
+    {
+        IAgeGroup ageGroup = new AgeGroup(value);
+        Assert.AreEqual("Indeterminado(a)", ageGroup.Text);
+    }
 }
